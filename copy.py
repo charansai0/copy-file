95434f17c415b4d0eff3869fe2c9d03c4e83e78d
charansai0 <ustrulycharan0@gmail.com>
2022-02-12 22:36:16 +0530

    https://github.com/charansai0/copy-file.git

--- copy.py
+++
@@ -1,7 +0,0 @@
-#  Developed by : v.charan sai
-#  Reference number : 21003158
-#  Copying of content from one text file to another text file.
-with open('text1','r') as file1:
-    with open('text2','w') as file2:
-        for i in file1:
-            file2.write(i)
